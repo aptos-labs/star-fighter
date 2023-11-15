2f733134ae7664a711638e6fc41a716057a201ba
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2023-11-15 11:31:31 -0800

    [game] Moved files around a bit

---
+++ game/Assets/3rdParty/Plugins/AsyncAwaitUtil/Source/WaitForUpdate.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+// This can be used as a way to return to the main unity thread when using multiple threads
+// with async methods
+public class WaitForUpdate : CustomYieldInstruction
+{
+  public override bool keepWaiting
+  {
+    get { return false; }
+  }
+}
